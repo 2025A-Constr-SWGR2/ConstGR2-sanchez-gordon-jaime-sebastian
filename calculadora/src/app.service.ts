508b1e392f98c99f731aefc095125a7034c83647
S3basjasan <jaime.sanchez02@epn.edu.ec>
2025-06-01 18:26:16 -0500

    workflows para los pasos de lint y test

--- calculadora/src/app.service.ts
+++ calculadora/src/app.service.ts
@@ -3,6 +3,11 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    // console.log('log!');
+    console.warn('warn!');
+    // console.info('info!');
+    // console.debug('debug!');
+    console.error('error!');
     return 'Hello World!';
   }
 }
